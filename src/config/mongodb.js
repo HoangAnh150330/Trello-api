1c78894f240afa55a004fd1fb3dae82666c624e9
Hoang Anh <hoanganhtt11@gmail.com>
2024-12-09 18:00:16 +0700

    Connect Mongodb

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,36 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+
+const MONGODB_URI ='mongodb://localhost:27017/'
+
+const DATABASE_NAME ='trello-hoanganh-mern-stack'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+//Khởi tạo một đối tượng trelloDatabseInstance ban đầu là null vì chưa connect
+let trelloDatabaseInstance = null
+
+//Khởi tạo một đối tượng  mongoClientInstance để connect tới MongoBD
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+  //Lư ý : cái Server API có từ phiên bản MONGODB 5.0.0 trở lên , có thể không cần dùng nó , nếu dùng nó là
+  // chúng ta sẽ chỉ định 1 cái Stable API Version cả Mongodb
+  // serverApi:{
+  //   version:ServerApiVersion.v1,
+  //   strict:true,
+  //   deprecationErrors:true
+  // }
+})
+
+//Kết nối Database
+export const CONNECT_DB = async () => {
+  //Gọi kết nối tới MongoDB Atlas với URI đã khai báo trong thân cả clientInstance
+  await mongoClientInstance.connect()
+  //Kết nối thành công thì lấy ra database theo tên và gán ngược nó lại vào trelloDatabaseInstance ở trên
+  trelloDatabaseInstance =mongoClientInstance.db(DATABASE_NAME)
+}
+
+// Function GET_DB có nhiệm vụ export ra cái trello Database Instance sau khi đã connect thành công
+//tới MongoDB để chúng ta sử dụng nhiều nơi khác nhau trong code
+//Lưu ý phải đảm bảo chỉ luôn gọi GET_DB này sau khi đã kết nối thành công tới Mongodb
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must Connect to Database first!')
+  return trelloDatabaseInstance
+}
--- src/server.js
+++ src/server.js
@@ -1,27 +1,44 @@
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import { CONNECT_DB, GET_DB } from '~/config/mongodb'
 
-const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+const START_SERVER = () => {
+  const app = express()
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  // eslint-disable-next-line no-console
-  console.log(mapOrder(
-    [{ id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' }],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  const hostname = 'localhost'
+  const port = 8017
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at http://${ hostname }:${ port }/`)
-})
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
+    res.end('<h1>Hello World!</h1><hr>')
+  })
+
+  app.listen(port, hostname, () => {
+    // eslint-disable-next-line no-console
+    console.log(`3.Hello Hoang Anh, Backend server is running at host:${hostname} and <Port:1>port</Port:1>`)
+  })
+}
+//Chỉ khi kết nối tới Database thành công thì mới có Start Server Backend lên
+//Immediately-invoked / Anonymous Async Function (IIFE)
+(async () => {
+  try{
+    console.log('1.Connecting to Mongodb....')
+    await CONNECT_DB()
+    console.log('2.Connected to MongoDB')
+    //Khởi động Backend khi connected database
+    START_SERVER()
+  }
+  catch(error) {
+    console.error(error)
+    process.exit(0)
+  }
+})()
+//Chỉ kết nối tới Database thành công thì mới Start Server backend -end lên
+// console.log('1.Connecting to Mongodb....')
+// CONNECT_DB()
+//   .then(() => console.log('2.Connected to MongoDB'))
+//   .then(() => START_SERVER() )
+//   .catch( error => {
+//     console.error(error)
+//     process.exit(0)
+//   })
