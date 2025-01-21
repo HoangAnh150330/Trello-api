e038a38f051b7c37f89b6d64bf4cdb690b94db83
Hoang Anh <hoanganhtt11@gmail.com>
2025-01-21 21:50:27 +0700

    Api create board

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,5 +1,6 @@
 import Joi from 'joi'
-
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validator'
+import { GET_DB } from '~/config/mongodb'
 //Define Collection (Name & Schema)
 const BOARD_COLLECTION_NAME= 'boards'
 const BOARD_COLLECTION_SCHEMA =Joi.object({
@@ -7,13 +8,38 @@ const BOARD_COLLECTION_SCHEMA =Joi.object({
   slug : Joi.string().required().min(3).trim().strict(),
   description : Joi.string().required().min(3).max(256).trim().strict(),
 
-  columnOrderIds: Joi.array().items(Joi.string()).default([]),
+  columnOrderIds: Joi.array().items(
+    Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
+  ).default([]),
   createdAt :Joi.date().timestamp('javascript').default(Date.now),
   updateAt :Joi.date().timestamp('javascript').default(null),
   _destroy : Joi.boolean().default(false)
 })
 
+const createNew = async (data) => {
+  try {
+    const createdBoard = await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(data)
+    return createdBoard
+    //return await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(data)
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+const findOneById = async (id) =>{
+  try {
+    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
+      _id : id
+    })
+    return result
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
 export const boardModel ={
   BOARD_COLLECTION_NAME,
-  BOARD_COLLECTION_SCHEMA
+  BOARD_COLLECTION_SCHEMA,
+  createNew,
+  findOneById
 }
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,20 +1,26 @@
 import { slugify } from '~/utils/formatters'
-
+import { boardModel } from '~/models/boardModel'
 const createNew = async(reqBody) => {
   // eslint-disable-next-line no-useless-catch
   try {
-    //Xử lý logic dữ liệu tùy đặc thù dự án 
+    //Xử lý logic dữ liệu tùy đặc thù dự án
     const newBoard = {
       ...reqBody,
       slug : slugify(reqBody.title)
     }
     //Goị tới tầng Model để xử lý lưu bản ghi newBoard vào trong Database
+    const createdBoard = await boardModel.createNew(newBoard)
+    console.log(createdBoard)
+
+    //Lấy bản ghi board sau khi gọi (tùy mục đích dự án mà có cần bước này hay không)
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
+    console.log(getNewBoard)
 
     //Làm thêm xử lý logic khác với các Collection khác tùy đặc thù dự án
     //Bắn email , notification về cho admin khi cái board mới được tạo
 
     //trả kết quả về , trong service luôn phải có return
-    return newBoard
+    return getNewBoard
   } catch (error) {
     throw error
   }
