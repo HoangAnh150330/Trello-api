0ad552fa70139b6b70b460a8b9bfa74c25765b58
Hoang Anh <hoanganhtt11@gmail.com>
2025-01-22 09:57:18 +0700

    api_get_detail_of_board

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -33,23 +33,29 @@ const createNew = async (data) => {
   }
 }
 
-const findOneById = async (id) =>{
+const findOneById = async (id) => {
   try {
-    console.log(id)
-    const testId = new ObjectId(id)
-    console.log(testId)
     const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
       _id : new ObjectId(id)
     })
     return result
-  } catch (error) {
-    throw new Error(error)
-  }
+  } catch (error) { throw new Error(error) }
+}
+
+// query tổng hợp (aggregate) để lấy toàn bộ Columns và cards thuộc về Board
+const getDetails = async (id) => {
+  try {
+    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
+      _id : new ObjectId(id)
+    })
+    return result
+  } catch (error) { throw new Error(error) }
 }
 
 export const boardModel ={
   BOARD_COLLECTION_NAME,
   BOARD_COLLECTION_SCHEMA,
   createNew,
-  findOneById
+  findOneById,
+  getDetails
 }
