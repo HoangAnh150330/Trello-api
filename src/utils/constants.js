01a025a4a82a09a9e0085153496aaedab32d0b20
Hoang Anh <hoanganhtt11@gmail.com>
2025-02-09 20:49:46 +0700

    Update Whitelist Domains.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,7 @@
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173'
   // Không cần localhost nữa vì ở file config/cors đã luôn cho phép môi trường dev (env.BUILD_MODE === 'dev')
+  'https://trello-web-seven-sandy.vercel.app/'
 ]
 export const BOARD_TYPES ={
   PUBLIC :'public',
